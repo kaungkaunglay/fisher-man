9209396c9fd46a36468aefbbf69f43c8ce541420
swethaharhtun <116005543+swethaharhtun@users.noreply.github.com>
2025-01-07 15:18:17 +0630

    updated support page

--- assets/js/support.js
+++ assets/js/support.js
@@ -1,21 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const accordionButtons = document.querySelectorAll(".accordion-button");
+  const accordionButtons = document.querySelectorAll(".accordion-button");
 
-    accordionButtons.forEach(button => {
-        button.addEventListener("click", function () {
-            const currentPanel = this.getAttribute("data-mdb-target");
-            const panels = document.querySelectorAll(".accordion-collapse");
+  accordionButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+      const currentPanel = this.getAttribute("data-mdb-target");
+      const panels = document.querySelectorAll(".accordion-collapse");
 
-            // Close all panels
-            panels.forEach(panel => {
-                if (panel.id !== currentPanel.substring(1)) {
-                    panel.classList.remove("show");
-                }
-            });
+      panels.forEach((panel) => {
+        if (panel.id !== currentPanel.substring(1)) {
+          panel.classList.remove("show");
+        }
+      });
 
-            // Toggle the clicked panel
-            const targetPanel = document.querySelector(currentPanel);
-            targetPanel.classList.toggle("show");
-        });
+      const targetPanel = document.querySelector(currentPanel);
+      targetPanel.classList.toggle("show");
     });
+  });
 });
