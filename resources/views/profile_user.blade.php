e881260a76ae5c3cf3b58e83b9c419633cf943c3
aunghtetpaingandfun <aunghtetpaing@andfun.biz>
2025-01-27 16:02:24 +0630

    Fix user profile

--- resources/views/profile_user.blade.php
+++ resources/views/profile_user.blade.php
@@ -3,142 +3,155 @@
 <link rel="stylesheet" href="{{ asset('assets/css/profile_user.css') }}" />
 @endsection
 @section('contents')
-<div class="breadcrumb">
-  <a href="#home">Home</a>
-  <a href="#profile">Profile</a>
-</div>
-<div class="row">
-  <!-- profile start -->
-  <div class="profile col-md-6 col-sm-12">
-    <!-- profile status start -->
-    <div class="profile-status row">
-      <div class="profile-img col-md-6 col-sm-12">
-        <img src="{{asset('assets/images/profile.jpg')}}" alt="">
+<!-- Breadcrumbs -->
+<nav aria-label="breadcrumb" class="py-4">
+  <div class="container">
+    <ol class="breadcrumb mb-0 bg-transparent">
+      <li class="breadcrumb-item"><a href="./home.html">Home</a></li>
+      <li class="breadcrumb-item active" aria-current="page">Porfile</li>
+    </ol>
+  </div>
+</nav>
+<!-- ./Breadcrumbs -->
+
+<!-- Main Content -->
+<div class="container">
+  <div class="row">
+    <div class="col -12 col-lg-6">
+      <div class="row">
+        <div class="col-12 col-lg-6">
+          <img src="{{asset('assets/images/profile.png')}}" class="w-100" alt="">
+        </div>
+        <div class="col-12 col-lg-6 mt-2 mt-lg-0">
+          <button class="w-100 d-block text-end "><i class="fa-solid fa-pen-to-square fs-5 text-danger "></i></button>
+          <h6 class="fw-bold">Name or Organization name</h6>
+          <p class="mb-0">email@gmail.com</p>
+          <p class="mb-0">LineID</p>
+          <p class="mb-0">Chat with name or Organization name</p>
+          <button><i class="fa-brands fa-line fs-2 mt-2"></i></button>
+        </div>
       </div>
-      <div class="profile-detail col-md-6 col-sm-12 d-flex flex-column">
-        <p>Danny</p>
-        <a href="mailto:#" class="mail" id="email">email@gmail.com</a>
-        <p class="line-acc">LineID</p>
-        <p>Chat with name or Organization name</p>
-        <div class="media-list list-group">
-          <a href=""><i class="fab fa-line"></i></a>
+      <div class="row mt-3">
+        <div class="col-12">
+          <h6 class="fw-bold mb-3 d-flex detail-title">
+            Detail Personal Info
+            <div class="col-3 text-end">
+              <button>
+                <i class="fa-solid fa-pen-to-square fs-5 text-danger"></i>
+              </button>
+            </div>
+          </h6>
+          <div class="address d-flex mb-1">
+            <p class="w-25 fw-semibold">Address</p>
+            <p class="ms-5 ps-3 ps-lg-0 text-black">:house no street,sue distict,city</p>
+          </div>
+          <div class="address d-flex mb-2">
+            <p class="w-25 fw-semibold">Phone No</p>
+            <p class="ms-5 text-black">:09429523324,0966723634</p>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12 col-lg-9">
+          <div class="buttons d-flex">
+            <button class="btn btn-outline-primary px-4 w-50 py-1">Register as a seller</button>
+            <button class="btn btn-outline-primary px-4 ms-2 w-50">Check order status</button>
+          </div>
         </div>
       </div>
     </div>
-    <!-- profile status end -->
-
-    <!-- address start -->
-    <address class="address">
-      <h2 class="title">Detail Personal Info</h2>
-      <div class="row">
-        <h3 class="title col-4">Address</h3>
-        <p class="profile-address col-8">: house no, street, sub-district, district, city</p>
-      </div>
-      <div class="row">
-        <h3 class="title col-4">Phone No</h3>
-        <ul class="profile-tele-list d-flex col-8">
-          <li>
-            <a href="tele:#">: <i class="tele" id="tel1">+959848499834</i></a>
-          </li>
-          <li>
-            <a href="tele:#">, <i class="tele" id="tel2">+959998199834</i></a>
-          </li>
-        </ul>
-      </div>
-    </address>
-    <!-- address end -->
-
-    <div class="btn-container d-flex">
-      <a href="" class="btn btn-outline-primary">Register as a seller</a>
-      <a href="" class="btn btn-outline-primary">check order status</a>
+    <div class="col-12 col-lg-1"></div>
+    <div class="col-12 col-lg-5 mt-3 mt-lg-0">
+      <h6 class="fw-bold">Select Your Location</h6>
+      <iframe class="w-100"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d250151.16276620553!2d104.72537013378734!3d11.579654014369655!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3109513dc76a6be3%3A0x9c010ee85ab525bb!2sPhnom%20Penh%2C%20Cambodia!5e0!3m2!1sen!2ssg!4v1736774811619!5m2!1sen!2ssg"
+        height="330" style="border:0;" allowfullscreen="" loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
-
   </div>
-  <!-- profile end -->
 
-  <div class="map col-md-6 col-sm-12">
-    <h2 class="title">Select Your Location</h2>
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d27815390.40349121!2d115.72585034042068!3d31.676717858944855!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x34674e0fd77f192f%3A0xf54275d47c665244!2z5pel5pys!5e0!3m2!1sja!2sjp!4v1736909758335!5m2!1sja!2sjp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+  <!-- History Table -->
+  <div class="history">
+    <h2 class="title">History</h2>
+    <ol class="history-list">
+      <li>
+        <div class="history-item row">
+          <div class="col-md-8 col-sm-12">
+            <h3 class="shop-name">Shop Name</h3>
+            <p class="payment">Payment method & used card last no.</p>
+          </div>
+          <div class="col-md-4 col-sm-12">
+            <span class="date">15/06/2024</span>
+            <a class="download" download="">
+              <i class="fa-solid fa-download"></i>
+            </a>
+          </div>
+        </div>
+      </li>
+
+      <li>
+        <div class="history-item row">
+          <div class="col-md-8 col-sm-12">
+            <h3 class="shop-name">Shop Name</h3>
+            <p class="payment">Payment method & used card last no.</p>
+          </div>
+          <div class="col-md-4 col-sm-12">
+            <span class="date">15/06/2024</span>
+            <a class="download" download="">
+              <i class="fa-solid fa-download"></i>
+            </a>
+          </div>
+        </div>
+      </li>
+
+      <li>
+        <div class="history-item row">
+          <div class="col-md-8 col-sm-12">
+            <h3 class="shop-name">Shop Name</h3>
+            <p class="payment">Payment method & used card last no.</p>
+          </div>
+          <div class="col-md-4 col-sm-12">
+            <span class="date">15/06/2024</span>
+            <a class="download" download="">
+              <i class="fa-solid fa-download"></i>
+            </a>
+          </div>
+        </div>
+      </li>
+
+      <li>
+        <div class="history-item row">
+          <div class="col-md-8 col-sm-12">
+            <h3 class="shop-name">Shop Name</h3>
+            <p class="payment">Payment method & used card last no.</p>
+          </div>
+          <div class="col-md-4 col-sm-12">
+            <span class="date">15/06/2024</span>
+            <a class="download" download="">
+              <i class="fa-solid fa-download"></i>
+            </a>
+          </div>
+        </div>
+      </li>
+
+      <li>
+        <div class="history-item row">
+          <div class="col-md-8 col-sm-12">
+            <h3 class="shop-name">Shop Name</h3>
+            <p class="payment">Payment method & used card last no.</p>
+          </div>
+          <div class="col-md-4 col-sm-12">
+            <span class="date">15/06/2024</span>
+            <a class="download" download="">
+              <i class="fa-solid fa-download"></i>
+            </a>
+          </div>
+        </div>
+      </li>
+    </ol>
   </div>
+  <!-- ./History Table -->
 </div>
+<!-- ./Main Content -->
 
-<div class="history">
-  <h2 class="title">History</h2>
-  <ol class="history-list">
-    <li>
-      <div class="history-item row">
-        <div class="col-md-8 col-sm-12">
-          <h3 class="shop-name">Shop Name</h3>
-          <p class="payment">Payment method & used card last no.</p>
-        </div>
-        <div class="col-md-4 col-sm-12">
-          <span class="date">15/06/2024</span>
-          <a class="download" download="">
-            <i class="fa-solid fa-download"></i>
-          </a>
-        </div>
-      </div>
-    </li>
-    
-    <li>
-      <div class="history-item row">
-        <div class="col-md-8 col-sm-12">
-          <h3 class="shop-name">Shop Name</h3>
-          <p class="payment">Payment method & used card last no.</p>
-        </div>
-        <div class="col-md-4 col-sm-12">
-          <span class="date">15/06/2024</span>
-          <a class="download" download="">
-            <i class="fa-solid fa-download"></i>
-          </a>
-        </div>
-      </div>
-    </li>
-
-    <li>
-      <div class="history-item row">
-        <div class="col-md-8 col-sm-12">
-          <h3 class="shop-name">Shop Name</h3>
-          <p class="payment">Payment method & used card last no.</p>
-        </div>
-        <div class="col-md-4 col-sm-12">
-          <span class="date">15/06/2024</span>
-          <a class="download" download="">
-            <i class="fa-solid fa-download"></i>
-          </a>
-        </div>
-      </div>
-    </li>
-
-    <li>
-      <div class="history-item row">
-        <div class="col-md-8 col-sm-12">
-          <h3 class="shop-name">Shop Name</h3>
-          <p class="payment">Payment method & used card last no.</p>
-        </div>
-        <div class="col-md-4 col-sm-12">
-          <span class="date">15/06/2024</span>
-          <a class="download" download="">
-            <i class="fa-solid fa-download"></i>
-          </a>
-        </div>
-      </div>
-    </li>
-
-    <li>
-      <div class="history-item row">
-        <div class="col-md-8 col-sm-12">
-          <h3 class="shop-name">Shop Name</h3>
-          <p class="payment">Payment method & used card last no.</p>
-        </div>
-        <div class="col-md-4 col-sm-12">
-          <span class="date">15/06/2024</span>
-          <a class="download" download="">
-            <i class="fa-solid fa-download"></i>
-          </a>
-        </div>
-      </div>
-    </li>
-  </ol>
-</div>
 @endsection
