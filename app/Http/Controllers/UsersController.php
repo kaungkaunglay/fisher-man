0063acd39163083c10310d98ca7ae55dc3dccc4d
Aung Khant Zin <kk8225248@gmail.com>
2025-01-27 15:58:51 +0630

    Updated

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,8 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Users;
-use Illuminate\Support\Facades\Auth;
+use App\Models\Users; 
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
@@ -79,14 +78,13 @@ class UsersController extends Controller
         if($validator->fails()){
             return response()->json(['status' => false, 'errors' => $validator->errors()]);
         }else{
-            // Attempt to find the user by username or email
+
             $user = Users::where('username', $request->username)
             ->orWhere('email', $request->username)
             ->first();
-
+            
             if($user && Hash::check($request->password, $user->password)){
-                Auth::login($user);
-                return response()->json(['status' => true, 'message' => 'Login success']);
+                return response()->json(['status' => true, 'message' => 'Login success', 'errors'=> '']);
             }
 
             return response()->json(['status' => false, 'message' => 'Username or Password is Incorrect']);
