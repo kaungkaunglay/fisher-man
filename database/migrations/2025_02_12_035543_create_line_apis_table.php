9d7860eb6b489b0425ddb96a2d24c70ab9f9a1ed
Aung Khant Zin <kk8225248@gmail.com>
2025-02-12 10:36:39 +0630

    Updated Api

--- database/migrations/2025_02_12_035543_create_line_apis_table.php
+++ database/migrations/2025_02_12_035543_create_line_apis_table.php
@@ -11,10 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('sub_categories', function (Blueprint $table) {
+        Schema::create('line_apis', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');
+            $table->string('line_user_id');
+            $table->string('events')->nullable();
             $table->timestamps();
         });
     }
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('sub_categories');
+        Schema::dropIfExists('line_apis');
     }
 };
