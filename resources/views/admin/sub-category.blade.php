9fe71cca42d912a348a61286a7434d72c11e96bc
swethaharhtun <116005543+swethaharhtun@users.noreply.github.com>
2025-02-14 13:39:15 +0630

    fix error

--- resources/views/admin/sub-category.blade.php
+++ resources/views/admin/sub-category.blade.php
@@ -19,7 +19,7 @@
             <h3>{{ isset($sub_category) ? 'Edit Sub-Category' : 'New Sub-Category' }}</h3>
             <ul class="breadcrumbs flex items-center flex-wrap justify-start gap10">
                 <li>
-                    <a href="{{ route('admin') }}"><div class="text-tiny">Dashboard</div></a>
+                    <a href="{{ route('admin.index') }}"><div class="text-tiny">Dashboard</div></a>
                 </li>
                 <li>
                     <i class="icon-chevron-right"></i>
