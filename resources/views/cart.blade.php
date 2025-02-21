d380377a41e819f5572ead5b04281d5211871688
Aung Khant Zin <kk8225248@gmail.com>
2025-02-21 07:54:04 +0630

    Fixed Bug

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -330,201 +330,198 @@
   </section>
   <!-- /Address Step -->
 
-  <!-- Payment Step -->
-  <section class="page" id="payment">
+ <!-- Payment Step -->
+<section class="page" id="payment">
     <div class="container-custom">
     
-      <!-- Payment Method Form -->
-      <div class="popup">
-        <div class="bg-white rounded-3 border text-black mx-auto" id="payment-form">
-          <h2 class="title">Add Payment Method</h2>
-          <form class="d-flex flex-column" action="">
-            <div>
-              <label for="card-number">Card number</label>
-              <div class="input-group border bg-white rounded">
-                <input type="number" name="" id="card-number" class="form-control border-0 p-2 shadow-none"
-                  placeholder="1234 1234 1234 1234">
-                <div class="input-group-append d-flex gap-1 p-2 align-item-center">
-                  <div class="card-bank-img align-content-center">
-                    <img src="{{ asset('assets/icons/custom/visa.svg') }}" alt="visa.svg">
-                  </div>
-                  <div class="card-bank-img align-content-center">
-                    <img src="{{ asset('assets/icons/custom/mastercard.svg') }}" alt="mastercard.svg">
-                  </div>
-                  <div class="card-bank-img align-content-center">
-                    <img src="{{ asset('assets/icons/custom/visa.svg') }}" alt="amex.svg">
-                  </div>
-                  <div class="card-bank-img align-content-center">
-                    <img src="{{ asset('assets/icons/custom/discover.svg') }}" alt="discover.svg">
-                  </div>
-                </div>
-              </div>
-            </div>
-  
-            <div class="d-flex flex-column flex-sm-row input-wpr">
-              <div class="w-100">
-                <label for="expire">Expiration</label>
-                <input type="date" name="" id="expire" class="w-100 p-2 border rounded">
-              </div>
-              <div class="w-100">
-                <label for="cvc">CVC</label>
-                <div class="input-group border bg-white rounded">
-                  <input type="number" name="" id="cvc" class="form-control border-0 p-2 shadow-none" placeholder="CVC">
-                  <div class="input-group-append p-2 align-content-center">
-                    <div class="card-bank-img">
-                      <img src="{{ asset('assets/icons/custom/discover.svg') }}" alt="visa.svg">
+        <!-- Payment Method Form -->
+        <div class="popup">
+            <div class="bg-white rounded-3 border text-black mx-auto" id="payment-form">
+                <h2 class="title">Add Payment Method</h2>
+                <form class="d-flex flex-column" action="">
+                    <div>
+                        <label for="card-number">Card number</label>
+                        <div class="input-group border bg-white rounded">
+                            <input type="number" name="" id="card-number" class="form-control border-0 p-2 shadow-none"
+                                placeholder="1234 1234 1234 1234">
+                            <div class="input-group-append d-flex gap-1 p-2 align-item-center">
+                                <div class="card-bank-img align-content-center">
+                                    <img src="{{ asset('assets/icons/custom/visa.svg') }}" alt="visa.svg">
+                                </div>
+                                <div class="card-bank-img align-content-center">
+                                    <img src="{{ asset('assets/icons/custom/mastercard.svg') }}" alt="mastercard.svg">
+                                </div>
+                                <div class="card-bank-img align-content-center">
+                                    <img src="{{ asset('assets/icons/custom/visa.svg') }}" alt="amex.svg">
+                                </div>
+                                <div class="card-bank-img align-content-center">
+                                    <img src="{{ asset('assets/icons/custom/discover.svg') }}" alt="discover.svg">
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                  </div>
+  
+                    <div class="d-flex flex-column flex-sm-row input-wpr">
+                        <div class="w-100">
+                            <label for="expire">Expiration</label>
+                            <input type="date" name="" id="expire" class="w-100 p-2 border rounded">
+                        </div>
+                        <div class="w-100">
+                            <label for="cvc">CVC</label>
+                            <div class="input-group border bg-white rounded">
+                                <input type="number" name="" id="cvc" class="form-control border-0 p-2 shadow-none" placeholder="CVC">
+                                <div class="input-group-append p-2 align-content-center">
+                                    <div class="card-bank-img">
+                                        <img src="{{ asset('assets/icons/custom/discover.svg') }}" alt="visa.svg">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+  
+                    <div class="d-flex flex-column flex-sm-row input-wpr">
+                        <div class="w-100">
+                            <label for="count">Country</label>
+                            <select id="count" class="w-100 p-2 border rounded">
+                                <option value="jpn" selected>Japan</option>
+                            </select>
+                        </div>
+                        <div class="w-100">
+                            <label for="zp">ZIP</label>
+                            <input name="" type="number" id="zp" class="w-100 p-2 border rounded" placeholder="104-0044">
+                        </div>
+                    </div>
+  
+                    <p class="content">
+                        By providing your card information, you allow Awardco, Inc. to charge your card for future
+                        payments in accordance with their terms.
+                    </p>
+                    <div class="d-flex align-items-start gap-2">
+                        <input type="checkbox" id="agree" class=" mt-2">
+                        <label for="agree">
+                            I agree that by saving this payment method it will be available for use to all who have
+                            access to this page or to processing funding deposits.
+                        </label>
+                    </div>
+  
+                    <div class="d-flex gap-3 text-center justify-content-center">
+                        <button class="common-btn btn btn-outline-primary" id="cancel">Cancel</button>
+                        <a href="#complete" class="common-btn btn btn-outline-primary btn-next">Save</a>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <!-- ./Payment Method Form -->
+  
+        <!-- Desktop Style -->
+        <table class="table desktop text-center d-md-table d-none table-item">
+            <thead>
+                <tr>
+                    <th scope="col">Image</th>
+                    <th scope="col">Product address</th>
+                    <th scope="col">Price</th>
+                    <th scope="col">Total</th>
+                </tr>
+            </thead>
+            <tbody class="dsk-cart-body">
+                @foreach ($carts as $item)
+                <tr class="table-row cart-{{ $item->product->id }}">
+                    <td>
+                        <div class="table-img"><img src="{{ asset($item->product->product_image) }}" alt="product img"></div>
+                    </td>
+                    <td class="col-name">{{ $item->product->name }}</td>
+                    <td class="price">¥{{ $item->product->product_price }}</td>
+                    <td class="cost">
+                        <input type="hidden" value="1" class="quantity-value">
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+            <tfoot>
+                <tr>
+                    <td colspan="2"></td>
+                    <td>Total</td>
+                    <td>
+                        <span class="total"></span>
+                    </td>
+                </tr>
+            </tfoot>
+        </table>
+        <!-- ./Desktop Style -->
+  
+        <!-- Mobile Style -->
+        <div class="mobile d-md-none d-flex flex-column gap-3 table-item mb-cart-body">
+            @foreach ($carts as $item)
+            <div class="card cart-{{ $item->product->id }}">
+                <div class="card-img align-content-center me-2">
+                    <img src="{{ asset($item->product->product_image) }}" alt="product img">
                 </div>
-              </div>
+                <div class="card-body">
+                    <div class="table-row">
+                        <p class="card-name">{{ $item->product->name }}</p>
+                        <div class="card-text">
+                            <span class="cost">
+                                <input type="hidden" value="1" class="quantity-value">
+                            </span>
+                            <span class="price">{{ $item->product->product_price }}</span>
+                        </div>
+                    </div>
+                </div>
             </div>
+            @endforeach
+            <div class="no-cart"></div>
   
-            <div class="d-flex flex-column flex-sm-row input-wpr">
-              <div class="w-100">
-                <label for="count">Country</label>
-                <select id="count" class="w-100 p-2 border rounded">
-                  <option value="jpn" selected>Japan</option>
-                </select>
-              </div>
-              <div class="w-100">
-                <label for="zp">ZIP</label>
-                <input name="" type="number" id="zp" class="w-100 p-2 border rounded" placeholder="104-0044">
-              </div>
+            <div class="d-flex justify-content-between bg-primary text-white p-2 mt-3">
+                <p>Total :</p>
+                <p>
+                    <span class="total"></span>
+                </p>
             </div>
-  
-            <p class="content">
-              By providing your card information, you allow Awardco, Inc. to charge your card for future
-              payments in
-              accordance with their terms.
-            </p>
-            <div class="d-flex align-items-start gap-2">
-              <input type="checkbox" id="agree" class=" mt-2">
-              <label for="agree">
-                I agree that by saving this payment method it will be available for use to all who have
-                access to this
-                page or to processing funding deposits.
-              </label>
-            </div>
-  
-            <div class="d-flex gap-3 text-center justify-content-center">
-              <button class="common-btn btn btn-outline-primary" id="cancel">Cancel</button>
-              <a href="#complete" class="common-btn btn btn-outline-primary btn-next">Save</a>
-            </div>
-          </form>
         </div>
-      </div>
-      <!-- ./Payment Method Form -->
+        <!-- ./Mobile Style -->
   
-      <!-- Desktop Style -->
-      <table class="table desktop text-center d-md-table d-none table-item">
-        <thead>
-          <tr>
-            <th scope="col">Image</th>
-            <th scope="col">Product address</th>
-            <th scope="col">Price</th>
-            <th scope="col">Total</th>
-          </tr>
-        </thead>
-        <tbody class="dsk-cart-body">
-          @foreach ($carts as $item)
-          <tr class="table-row cart-{{ $item->product->id }}">
-            <td>
-              <div class="table-img"><img src="{{ asset($item->product->product_image) }}" alt="product img"></div>
-            </td>
-            <td clas="col-name">{{ $item->product->name }}</td>
-            <td class="price">¥{{ $item->product->product_price }}</td>
-            <td class="cost">
-              <input type="hidden" value="1" class="quantity-value">
-            </td>
-            </td>
-          </tr>
-          @endforeach>
-        </tbody>
-        <tfoot>
-          <tr>
-            <td colspan="2"></td>
-            <td>Total</td>
-            <td>
-              <span class="total"></span>
-            </td>
-          </tr>
-        </tfoot>
-      </table>
-      <!-- ./Desktop Style -->
-  
-      <!-- Mobile Style -->
-      <div class="mobile d-md-none d-flex flex-column gap-3 table-item mb-cart-body">
-        @foreach ($carts as $item)
-        <div class="card cart-{{ $item->product->id }}">
-          <div class="card-img align-content-center me-2">
-            <img src="{{ asset($item->product->product_image) }}" alt="product img">
-          </div>
-          <div class="card-body">
-            <div class="table-row">
-              <p class="card-name">{{ $item->product->name }}</p>
-              <div class="card-text">
-                <span class="cost">
-                  <input type="hidden" value="1" class="quantity-value">
-                </span>
-                <span class="price">{{ $item->product->product_price }}</span>
-              </div>
-            </div>
-          </div>
+        <h2 class="py-3 px-3 mt-5 bg-primary text-white" id="payment-check-sec">Select Payment</h2>
+        <div class="d-flex gap-3 py-3 px-3">
+            <input type="checkbox" id="select-payment">
+            <label for="select-payment">Credit Card</label>
+            <div class="ms-auto text-danger" id="warning-msg">Please Check the mark</div>
         </div>
-        @endforeach
-        <div class="no-cart"></div>
   
-        <div class="d-flex justify-content-between bg-primary text-white p-2 mt-3">
-          <p>Total :</p>
-          <p>
-            <span class="total"></span>
-          </p>
+        <h2 class="py-3 px-3 bg-primary text-white">Address</h2>
+        <ul class="list-group gap-3 py-3 px-3">
+            <li>
+                <span>Name </span>
+                : <span id="name-result"></span>
+            </li>
+            <li>
+                <span>Phone Number </span>
+                : <span id="tel-result"></span>
+            </li>
+            <li>
+                <span>Line ID </span>
+                : <span id="line_id-result"></span>
+            </li>
+            <li>
+                <span>Postal Code </span>
+                : 110001
+            </li>
+            <li>
+                <span>Country </span>
+                : Japan
+            </li>
+            <li>
+                <span>Delivery Address </span>
+                : <span id="delivery-result"></span>
+            </li>
+        </ul>
+        <div class="d-flex gap-3 my-4 justify-content-end">
+            <a href="#address" class="btn btn-outline-primary common-btn btn-back">Go Back</a>
+            <button class="btn btn-outline-primary common-btn btn-payment">Check Out</button>
         </div>
-      </div>
-      <!-- ./Mobile Style -->
-  
-      <h2 class="py-3 px-3 mt-5 bg-primary text-white" id="payment-check-sec">Select Payment</h2>
-      <div class="d-flex gap-3 py-3 px-3">
-        <input type="checkbox" id="select-payment">
-        <label for="select-payment">Credit Card</label>
-        <div class="ms-auto text-danger" id="warning-msg">Please Check the mark</div>
-      </div>
-  
-      <h2 class="py-3 px-3 bg-primary text-white">Address</h2>
-      <ul class="list-group gap-3 py-3 px-3">
-        <li>
-          <span>Name </span>
-          : <span id="name-result"></span>
-        </li>
-        <li>
-          <span>Phone Number </span>
-          : <span id="tel-result"></span>
-        </li>
-        <li>
-          <span>Line ID </span>
-          : <span id="line_id-result"></span>
-        </li>
-        <li>
-          <span>Postal Code </span>
-          : 110001
-        </li>
-        <li>
-          <span>Country </span>
-          : Japan
-        </li>
-        <li>
-          <span>Delivery Address </span>
-          : <span id="delivery-result"></span>
-        </li>
-      </ul>
-      <div class="d-flex gap-3 my-4 justify-content-end">
-        <a href="#address" class="btn btn-outline-primary common-btn btn-back">Go Back</a>
-        <button class="btn btn-outline-primary common-btn btn-payment">Check Out</button>
-      </div>
 
     </div>
-  </section>
-  <!-- /Payment Step -->
+</section>
+<!-- /Payment Step -->
 
   <!-- Complete Step -->
   <section class="page mt-4" id="complete">
