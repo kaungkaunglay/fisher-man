c889958721b5ad84bc72ad4d7ebee38f37aac4af
myatkoaung-afy <myatkoaung.andfun@gmail.com>
2025-02-21 18:09:37 +0900

    latest

---
+++ database/factories/PermissionFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Faker\Generator as Faker;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Permission>
+ */
+class PermissionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+           //
+        ];
+    }
+}
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -30,7 +30,7 @@
                             </div>
                             <div>
                                 <div class="body-text mb-2">Total Product</div>
-                                <h4>34,945</h4>
+                                <h4>{{ $total_products->count() }}</h4>
                             </div>
                         </div>
                         {{-- <div class="box-icon-trending up">
@@ -68,24 +68,24 @@
                            @foreach($top_products as $top_product)
                            <li class="product-item">
                             <div class="image">
-                                <img src="{{asset('$top_product->product_image')}}" alt="">
+                                <img src="{{asset($top_product->product_image)}}" alt="">
                             </div>
                             <div class="flex items-center justify-between flex-grow">
                                 <div class="name">
-                                    <a href="product-list.html" class="body-title-2">Patimax Fragrance Long...</a>
-                                    <div class="text-tiny mt-3">100 ¥</div>
+                                    <a href="product-list.html" class="body-title-2">{{$top_product->name}}</a>
+                                    <div class="text-tiny mt-3">¥ {{$top_product->product_price}}</div>
                                 </div>
                                 <div>
                                     <div class="body-title-2 mb-3">Discount</div>
-                                    <div class="text-tiny">2.71</div>
+                                    <div class="text-tiny">{{$top_product->discount}}</div>
                                 </div>
                                 <div class="seller-name">
                                     <div class="text-tiny mb-3">Seller</div>
-                                    <div class="body-text">Rose</div>
+                                    <div class="body-text">{{$top_product->username}}</div>
                                 </div>
                                 <div class="stock">
                                     <div class="text-tiny mb-3">Stock</div>
-                                    <div class="body-text">sadfasdfasdfasdf</div>
+                                    <div class="body-text">{{$top_product->stock}}</div>
                                 </div>
                             </div>
                         </li>
@@ -99,20 +99,7 @@
                 <!-- product-overview -->
                 <div class="wg-box">
                     <div class="flex items-center justify-between">
-                        <h5>Product overview</h5>
-                        <div class="dropdown default">
-                            <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="view-all">View all<i class="icon-chevron-down"></i></span>
-                            </button>
-                            <ul class="dropdown-menu dropdown-menu-end">
-                                <li>  
-                                    <a href="javascript:void(0);">3 days</a>
-                                </li>
-                                <li>  
-                                    <a href="javascript:void(0);">7 days</a>
-                                </li>
-                            </ul>
-                        </div>
+                        <h5>All Product overview</h5>
                     </div>
                     <div class="wg-table table-product-overview">
                         <ul class="table-title flex gap20 mb-14">
@@ -120,111 +107,79 @@
                                 <div class="body-title">Name</div>
                             </li>    
                             <li>
-                                <div class="body-title">Product ID</div>
-                            </li>
-                            <li>
                                 <div class="body-title">Price</div>
                             </li>
                             <li>
-                                <div class="body-title">Quantity</div>
+                                <div class="body-title">Stock</div>
                             </li>
                             <li>
                                 <div class="body-title">Sale</div>
                             </li>
                             <li>
-                                <div class="body-title">Revenue</div>
+                                <div class="body-title">Seller Name</div>
                             </li>
                             <li>
-                                <div class="body-title">Status</div>
+                                <div class="body-title">Imported Date</div>
                             </li>
                         </ul>
                         <ul class="flex flex-column gap10">
+                            @foreach($all_products as $product)
                             <li class="product-item gap14">
                                 <div class="image">
-                                    <img src="{{asset('assets/admin/images/products/6.png')}}" alt="">
+                                    <img src="{{asset($product->product_image)}}" alt="">
                                 </div>
                                 <div class="flex items-center justify-between flex-grow">
                                     <div class="name">
-                                        <a href="product-list.html" class="body-text">Soft Fluffy Cats</a>
+                                        <a href="#" class="body-text">{{$product->name}}</a>
                                     </div>
-                                    <div class="body-text">#327</div>
-                                    <div class="body-text">$11.70</div>
-                                    <div class="body-text">28</div>
+                                    <div class="body-text">¥{{$product->product_price}}</div>
+                                    <div class="body-text">{{$product->stock}}</div>
                                     <div class="body-text">On sale</div>
-                                    <div class="body-text">$328.85</div>
-                                    <div class="block-not-available">Not Available</div>
+                                    <div class="body-text">{{$product->username}}</div>
+                                    <div class="body-text">{{$product->created_at->format('Y-m-d')}}</div>
                                 </div>
                             </li>
-                            <li class="product-item gap14">
-                                <div class="image">
-                                    <img src="{{asset('assets/admin/images/products/7.png')}}" alt="">
-                                </div>
-                                <div class="flex items-center justify-between flex-grow">
-                                    <div class="name">
-                                        <a href="product-list.html" class="body-text">Taste of the Wild Formula Finder</a>
-                                    </div>
-                                    <div class="body-text">#380</div>
-                                    <div class="body-text">$8.99</div>
-                                    <div class="body-text">10</div>
-                                    <div class="body-text">On sale</div>
-                                    <div class="body-text">$105.55</div>
-                                    <div class="block-not-available">Not Available</div>
-                                </div>
-                            </li>
-                            <li class="product-item gap14">
-                                <div class="image">
-                                    <img src="{{asset('assets/admin/images/products/8.png')}}" alt="">
-                                </div>
-                                <div class="flex items-center justify-between flex-grow">
-                                    <div class="name">
-                                        <a href="product-list.html" class="body-text">Wellness Natural Food</a>
-                                    </div>
-                                    <div class="body-text">#126</div>
-                                    <div class="body-text">$5.22</div>
-                                    <div class="body-text">578</div>
-                                    <div class="body-text">--/--</div>
-                                    <div class="body-text">$202.87</div>
-                                    <div class="block-not-available">Not Available</div>
-                                </div>
-                            </li>
-                            <li class="product-item gap14">
-                                <div class="image">
-                                    <img src="{{asset('assets/admin/images/products/9.png')}}" alt="">
-                                </div>
-                                <div class="flex items-center justify-between flex-grow">
-                                    <div class="name">
-                                        <a href="product-list.html" class="body-text">Dog Food Rachael Ray</a>
-                                    </div>
-                                    <div class="body-text">#582</div>
-                                    <div class="body-text">$14.81</div>
-                                    <div class="body-text">36</div>
-                                    <div class="body-text">--/--</div>
-                                    <div class="body-text">$475.22</div>
-                                    <div>
-                                        <div class="block-available">Available</div>
-                                    </div>
-                                </div>
-                            </li>
-                            <li class="product-item gap14">
-                                <div class="image">
-                                    <img src="{{asset('assets/admin/images/products/10.png')}}" alt="">
-                                </div>
-                                <div class="flex items-center justify-between flex-grow">
-                                    <div class="name">
-                                        <a href="product-list.html" class="body-text">Best Buddy Bits Dog Treats</a>
-                                    </div>
-                                    <div class="body-text">#293</div>
-                                    <div class="body-text">$6.48</div>
-                                    <div class="body-text">84</div>
-                                    <div class="body-text">--/--</div>
-                                    <div class="body-text">$219.78</div>
-                                    <div class="block-not-available">Not Available</div>
-                                </div>
-                            </li>
+                            @endforeach
                         </ul>
                     </div>
                     <div class="divider"></div>
+                    {{-- <div class="d-flex justify-content-center">
+                        {{ $all_products->links() }}
+                    </div> --}}
+                    <!-- Custom Pagination -->
+                    @if ($all_products->hasPages())
                     <div class="flex items-center justify-between flex-wrap gap10">
+                        <div class="text-tiny">
+                            Showing {{ $all_products->firstItem() }} to {{ $all_products->lastItem() }} of {{ $all_products->total() }} entries
+                        </div>
+                        <ul class="wg-pagination">
+                            <!-- Previous Page -->
+                            <li class="{{ $all_products->onFirstPage() ? 'disabled' : '' }}">
+                                <a href="{{ $all_products->previousPageUrl() }}">
+                                    <i class="icon-chevron-left"></i>
+                                </a>
+                            </li>
+                    
+                            <!-- Page Numbers -->
+                            @foreach ($all_products->links()->elements[0] as $page => $url)
+                                <li class="{{ $page == $all_products->currentPage() ? 'active' : '' }}">
+                                    <a href="{{ $url }}">{{ $page }}</a>
+                                </li>
+                            @endforeach
+                    
+                            <!-- Next Page -->
+                            <li class="{{ $all_products->hasMorePages() ? '' : 'disabled' }}">
+                                <a href="{{ $all_products->nextPageUrl() }}">
+                                    <i class="icon-chevron-right"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    @endif
+                    
+                    
+                    
+                    {{-- <div class="flex items-center justify-between flex-wrap gap10">
                         <div class="text-tiny">Showing 5 entries</div>
                         <ul class="wg-pagination">
                             <li>
@@ -243,7 +198,7 @@
                                 <a href="#"><i class="icon-chevron-right"></i></a>
                             </li>
                         </ul>
-                    </div>
+                    </div> --}}
                 </div>
                 <!-- /product-overview -->
             </div>
