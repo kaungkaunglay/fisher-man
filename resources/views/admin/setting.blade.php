d024c1a98231314b104869c18b9ed47605ac40b7
Aung Khant Zin <kk8225248@gmail.com>
2025-03-05 10:44:25 +0630

    Updated

--- resources/views/admin/setting.blade.php
+++ resources/views/admin/setting.blade.php
@@ -1354,15 +1354,12 @@
                                 </form>
                                 <!-- /setting -->
                             </div>
-                            <!-- /main-content-wrap -->
-                        </div>
+
                         <!-- /main-content-wrap -->
                         <!-- bottom-page -->
-                        <div class="bottom-page">
-                            <div class="body-text">Copyright © 2024 Remos. Design with</div>
-                            <i class="icon-heart"></i>
-                            <div class="body-text">by <a href="https://themeforest.net/user/themesflat/portfolio">Themesflat</a> All rights reserved.</div>
-                        </div>
+                       <div class="bottom-page">
+    <div class="body-text">Copyright © 2025 r-mekiki.com, All rights reserved.</div>
+</div>
                         <!-- /bottom-page -->
                     </div>
                     <!-- /main-content -->
