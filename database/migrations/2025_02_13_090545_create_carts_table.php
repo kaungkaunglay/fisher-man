c2c81020735098c7abe47182e13c1c32b893ed07
zwehtet <zwehtetnaing@andfun.biz>
2025-03-06 12:52:09 +0630

    cart error fixing

--- database/migrations/2025_02_13_090545_create_carts_table.php
+++ database/migrations/2025_02_13_090545_create_carts_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->foreignId('product_id')->constrained('products')->onDelete('cascade');
-            $table->integer('quantity')->default(1);
+            $table->integer('quantity')->default(0);
             $table->timestamps();
         });
     }
