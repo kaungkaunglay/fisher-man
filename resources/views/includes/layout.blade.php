c0b4717f851fba6ab5009f87c82d3945338c19d9
Zwe Htet <zwehtetnaing@andfun.biz>
2025-03-13 15:57:58 +0630

    favicon edit

--- resources/views/includes/layout.blade.php
+++ resources/views/includes/layout.blade.php
@@ -28,8 +28,8 @@
     <!-- {{-- favicon --}} -->
 
     <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/images/favicon/apple-touch-icon.png') }}">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/images/favicon/favicon-32x32.png') }}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/images/favicon/favicon-16x16.png') }}">
+    <link rel="icon" type="image/png"  href="{{ asset('assets/images/favicon/favicon.png') }}">
+    {{-- <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/images/favicon/favicon-16x16.png') }}"> --}}
     <script src="https://www.google.com/recaptcha/api.js" async defer></script>
 </head>
 
