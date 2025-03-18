809c28c2c168e89580635c58efd313f1cc493213
kaungkhantzin <you@example.com>
2025-03-18 12:47:53 +0630

    number range change

--- public/assets/js/view-list.js
+++ public/assets/js/view-list.js
@@ -25,3 +25,85 @@
       });
     });
   });
+
+  //number range change /////
+
+  window.addEventListener("load", function () {
+    const slider1 = document.getElementById("slider-1");
+    const slider2 = document.getElementById("slider-2");
+    const minPriceInput = document.getElementById("min-price-input");
+    const maxPriceInput = document.getElementById("max-price-input");
+    const minPriceHidden = document.getElementById("min-price-hidden");
+    const maxPriceHidden = document.getElementById("max-price-hidden");
+    const track = document.querySelector(".slider-track");
+    const form = document.getElementById("priceRangeForm");
+
+    function formatNumberWithCommas(number) {
+        return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+
+    function parseFormattedNumber(string) {
+        return parseInt(string.replace(/,/g, ""));
+    }
+
+    function updateSlider() {
+        let percent1 = ((slider1.value - slider1.min) / (slider1.max - slider1.min)) * 100;
+        let percent2 = ((slider2.value - slider1.min) / (slider1.max - slider1.min)) * 100;
+
+        track.style.background = `linear-gradient(to right, #dadae5 ${percent1}%, #3264fe ${percent1}%, #3264fe ${percent2}%, #dadae5 ${percent2}%)`;
+        minPriceInput.value = formatNumberWithCommas(slider1.value);
+        maxPriceInput.value = formatNumberWithCommas(slider2.value);
+        minPriceHidden.value = slider1.value;
+        maxPriceHidden.value = slider2.value;
+    }
+
+    function handleTextInput(input, slider, hiddenInput) {
+        input.addEventListener("input", function (e) {
+            let value = e.target.value.replace(/[^\d,]/g, "");
+            value = value.replace(/,/g, "");
+
+            if (value === "") value = "0";
+            let numValue = parseInt(value);
+            if (numValue < 1) numValue = 1;
+            if (numValue > 50000) numValue = 50000;
+            slider.value = numValue;
+            hiddenInput.value = numValue;
+            e.target.value = formatNumberWithCommas(numValue);
+
+            updateSlider();
+        });
+        input.addEventListener("blur", function (e) {
+            let value = parseFormattedNumber(e.target.value);
+            e.target.value = formatNumberWithCommas(value);
+            hiddenInput.value = value;
+        });
+    }
+
+    slider1.addEventListener("input", function () {
+        const minVal = parseInt(slider1.value);
+        const maxVal = parseInt(slider2.value);
+
+        if (minVal > maxVal) {
+            slider1.value = maxVal;
+        }
+        updateSlider();
+    });
+    slider2.addEventListener("input", function () {
+        const minVal = parseInt(slider1.value);
+        const maxVal = parseInt(slider2.value);
+
+        if (maxVal < minVal) {
+            slider2.value = minVal;
+        }
+        updateSlider();
+    });
+
+    handleTextInput(minPriceInput, slider1, minPriceHidden);
+    handleTextInput(maxPriceInput, slider2, maxPriceHidden);
+
+    [slider1, slider2].forEach((slider) => {
+        slider.addEventListener("change", () => form.submit());
+    });
+
+    updateSlider();
+});
--- resources/views/sub_category.blade.php
+++ resources/views/sub_category.blade.php
@@ -1,190 +1,154 @@
-@extends('includes.layout')
-@section('style')
-    <link rel="stylesheet" href="{{ asset('assets/css/sub_category.css') }}" />
-    <link rel="stylesheet" href="{{ asset('assets/css/home.css') }}" />
-@endsection
-@section('contents')
-    <div class="breadcrumb-sp mt-4 mb-3"></div>
-
-    <section class="hero container-custom">
-        <div class="row justify-content-between">
-            <div class="col-lg-4 col-md-6 d-none d-lg-block">
-                @include('includes.aside')
-            </div>
-            <div class="col-lg-8 col-md-12">
-                @include('includes.slider')
-            </div>
+@extends('includes.layout') @section('style')
+<link rel="stylesheet" href="{{ asset('assets/css/sub_category.css') }}" />
+<link rel="stylesheet" href="{{ asset('assets/css/home.css') }}" />
+@endsection @section('contents')
+<div class="breadcrumb-sp mt-4 mb-3"></div>
+<section class="hero container-custom">
+    <div class="row justify-content-between">
+        <div class="col-lg-4 col-md-6 d-none d-lg-block">
+            @include('includes.aside')
         </div>
-    </section>
-
-    <div class="container-custom">
-        <nav aria-label="breadcrumb" class="py-4">
-            <ol class="breadcrumb mb-0 bg-transparent">
-                <li class="breadcrumb-item"><a href="{{ route('home') }}">{{ trans_lang('home') }}</a></li>
-                <li class="breadcrumb-item" aria-current="page"><a
-                        href="{{ route('category', $subCategory->category_id) }}">{{ $subCategory->category->category_name }}</a>
-                </li>
-                <li class="breadcrumb-item" aria-current="page">{{ $subCategory->name }}</li>
-            </ol>
-        </nav>
-
-        <div class="sub-category mb-3">
-            <h3 class="title text-center m-b-20">{{ $subCategory->name }}</h3>
-
-            <div class="filter d-flex justify-content-between align-items-center mb-3">
-                <div class="icon-buttons txt-primary d-flex gap-3 align-items-center">
-                    <i class="fa-solid fa-grip fs-2 fw-bold" id="card-list-btn"></i>
-                    <i class="fa-solid fa-list fs-3 fw-bold" id="row-list-btn"></i>
-                </div>
-
-                <div class="sort-container">
-                    <div class="arrows">
-                        <button><i class="fa-solid fa-caret-up"></i></button>
-                        <button><i class="fa-solid fa-caret-down"></i></button>
-                    </div>
-                    <div class="dropdown">
-                        <button class="sort-button dropdown-toggle" type="button" data-bs-toggle="dropdown"
-                            aria-expanded="false">
-                            {{ trans_lang('sortby') }}
-                        </button>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item"
-                                    href="{{ route('sub-category.show', ['id' => $subCategory->id, 'sort_by' => 'price_asc']) }}">{{ trans_lang('price_l_h') }}</a>
-                            </li>
-                            <li><a class="dropdown-item"
-                                    href="{{ route('sub-category.show', ['id' => $subCategory->id, 'sort_by' => 'price_desc']) }}">{{ trans_lang('price_h_l') }}</a>
-                            </li>
-                            <li><a class="dropdown-item"
-                                    href="{{ route('sub-category.show', ['id' => $subCategory->id, 'sort_by' => 'name_asc']) }}">{{ trans_lang('name_a_z') }}</a>
-                            </li>
-                            <li><a class="dropdown-item"
-                                    href="{{ route('sub-category.show', ['id' => $subCategory->id, 'sort_by' => 'name_desc']) }}">{{ trans_lang('name_z_a') }}</a>
-                            </li>
-                            <li><a class="dropdown-item"
-                                    href="{{ route('sub-category.show', ['id' => $subCategory->id, 'sort_by' => 'latest']) }}">{{ trans_lang('latest') }}</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-
-            <form action="{{ route('sub-category.show', $subCategory->id) }}" method="GET" id="priceRangeForm">
-                <div class="range-slider mx-auto">
-                    <input type="number" name="min_price" class="min-price" value="{{ request('min_price', 1) }}"
-                        min="1" max="50000">
-                    <h3 class="txt">価格で絞り込み</h3>
-                    <input type="number" name="max_price" class="max-price" value="{{ request('max_price', 10000) }}"
-                        min="1" max="50000">
-
-                    <div class="range-container position-relative mt-3 w-100">
-                        <div class="slider-track"></div>
-                        <input type="range" min="1" max="50000" value="{{ request('min_price', 1) }}"
-                            id="slider-1">
-                        <input type="range" min="1" max="50000" value="{{ request('max_price', 10000) }}"
-                            id="slider-2">
-                    </div>
-                </div>
-            </form>
-
-
-
-
-
-            <div class="sub-category-item">
-                <div class="card-list" id="view-list">
-                    @if ($products && $products->isNotEmpty())
-                        @foreach ($products as $product)
-                            @if ($product->status == 'approved')
-                            <div class="item-card">
-                                <a href="{{ route('product.show', $product->id) }}" class="right">
-                                    <img loading="lazy" src="{{ asset('assets/products/' . $product->product_image) }}"
-                                        class="card-img-top" alt="{{ $product->name }}">
-                                </a>
-                                <div class="left pt-3">
-                                    {{-- <p class="price">¥{{ number_format($product->product_price) }}</p> --}}
-                                    <p class="price">
-                                        @if ($product->discount > 0)
-                                            <span class="format me-2">{{ number_format($product->product_price - $product->discount, 0) }}</span>
-                                            <span class="original-price format">{{ number_format($product->product_price, 0) }}</span>
-                                        @else
-                                            <span class="format">{{ number_format($product->product_price, 0) }}</span>
-                                        @endif
-                                    </p>
-                                    <div class="title-category mb-2">
-                                        <a href="{{ route('sub-category.show', $product->subCategory->id) }}"
-                                            class="menu-category">{{ $product->subCategory->name }}</a>
-                                        <h3 class="title">{{ $product->name }}</h3>
-                                    </div>
-                                    <a href="{{ route('product.show', $product->id) }}" class="txt m-b-10 description">
-                                        {{ $product->description }}
-                                    </a>
-                                    <div class="d-flex gap-2 card-btn m-t-10">
-                                        <a href="javascript:void(0);"
-                                            class="py-1 common-btn2 -solid cart-btn"
-                                            data-id="{{ $product->id }}">
-                                            <i class="fa-solid fa-cart-shopping"></i>
-                                        </a>
-                                        <a href="javascript:void(0);"
-                                            class="py-1 common-btn2 white-list-btn @if ($product->inWhiteLists()) active @endif"
-                                            data-id="{{ $product->id }}">
-                                            <i class="fa-solid fa-bookmark"></i>
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                            @endif
-                        @endforeach
-                    @else
-                        <h3 class="title">{{ trans_lang('no_products') }}</h3>
-                    @endif
-                </div>
-            </div>
-
-            <!-- Pagination -->
-            <div class="row mt-4">
-                @if ($products->hasPages())
-                    <ul class="pagination">
-                        <!-- Previous Page Link -->
-                        @if ($products->onFirstPage())
-                            <li class="disabled">&lt;</li>
-                        @else
-                            <li><a href="{{ $products->previousPageUrl() }}" class="">&lt;</a></li>
-                        @endif
-
-                        <!-- Page Numbers (Only Show 3 Pages at a Time) -->
-                        @php
-                            $start = max(1, $products->currentPage() - 1);
-                            $end = min($start + 2, $products->lastPage());
-                        @endphp
-
-                        @for ($i = $start; $i <= $end; $i++)
-                            <li class="{{ $products->currentPage() == $i ? 'active' : '' }}">
-                                <a href="{{ $products->url($i) }}" class="">{{ $i }}</a>
-                            </li>
-                        @endfor
-
-                        <!-- Next Page Link -->
-                        @if ($products->hasMorePages())
-                            <li><a href="{{ $products->nextPageUrl() }}" class="">&gt;</a></li>
-                        @else
-                            <li class="disabled">&gt;</li>
-                        @endif
-                    </ul>
-                @endif
-            </div>
-            {{-- <div class="pagination mt-2">
-                {{ $products->links('vendor.pagination.bootstrap-4') }}
-            </div> --}}
-            {{-- {{ $products->links() }} --}}
+        <div class="col-lg-8 col-md-12">
+            @include('includes.slider')
         </div>
     </div>
+</section>
 
-    <script src="{{ asset('assets/js/slider.js') }}"></script>
-    <script src="{{ asset('assets/js/price-range.js') }}"></script>
-    <script src="{{ asset('assets/js/pagination.js') }}"></script>
-    <script src="{{ asset('assets/js/notify.js') }}"></script>
+<div class="container-custom">
+    <nav aria-label="breadcrumb" class="py-4">
+        <ol class="breadcrumb mb-0 bg-transparent">
+            <li class="breadcrumb-item"><a href="{{ route('home') }}">{{ trans_lang('home') }}</a></li>
+            <li class="breadcrumb-item" aria-current="page"><a href="{{ route('category', $subCategory->category_id) }}">{{ $subCategory->category->category_name }}</a></li>
+            <li class="breadcrumb-item" aria-current="page">{{ $subCategory->name }}</li>
+        </ol>
+    </nav>
 
-    <!-- <script>
+    <div class="sub-category mb-3">
+        <h3 class="title text-center m-b-20">{{ $subCategory->name }}</h3>
+
+        <div class="filter d-flex justify-content-between align-items-center mb-3">
+            <div class="icon-buttons txt-primary d-flex gap-3 align-items-center">
+                <i class="fa-solid fa-grip fs-2 fw-bold" id="card-list-btn"></i>
+                <i class="fa-solid fa-list fs-3 fw-bold" id="row-list-btn"></i>
+            </div>
+
+            <div class="sort-container">
+                <div class="arrows">
+                    <button><i class="fa-solid fa-caret-up"></i></button>
+                    <button><i class="fa-solid fa-caret-down"></i></button>
+                </div>
+                <div class="dropdown">
+                    <button class="sort-button dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        {{ trans_lang('sortby') }}
+                    </button>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="{{ route('sub-category.show', ['id' => $subCategory->id, 'sort_by' => 'price_asc']) }}">{{ trans_lang('price_l_h') }}</a></li>
+                        <li><a class="dropdown-item" href="{{ route('sub-category.show', ['id' => $subCategory->id, 'sort_by' => 'price_desc']) }}">{{ trans_lang('price_h_l') }}</a></li>
+                        <li><a class="dropdown-item" href="{{ route('sub-category.show', ['id' => $subCategory->id, 'sort_by' => 'name_asc']) }}">{{ trans_lang('name_a_z') }}</a></li>
+                        <li><a class="dropdown-item" href="{{ route('sub-category.show', ['id' => $subCategory->id, 'sort_by' => 'name_desc']) }}">{{ trans_lang('name_z_a') }}</a></li>
+                        <li><a class="dropdown-item" href="{{ route('sub-category.show', ['id' => $subCategory->id, 'sort_by' => 'latest']) }}">{{ trans_lang('latest') }}</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <form action="{{ route('sub-category.show', $subCategory->id) }}" method="GET" id="priceRangeForm">
+            <div class="range-slider mx-auto">
+                <input type="hidden" name="min_price" id="min-price-hidden" />
+                <input type="hidden" name="max_price" id="max-price-hidden" />
+                <input type="text" class="min-price" id="min-price-input" value="{{ number_format((float) request('min_price', 1)) }}" min="1" max="50000" />
+                <h3 class="txt">価格で絞り込み</h3>
+                <input type="text" class="max-price" id="max-price-input" value="{{ number_format((float) request('max_price', 10000)) }}" min="1" max="50000" />
+
+                <div class="range-container position-relative mt-3 w-100">
+                    <div class="slider-track"></div>
+                    <input type="range" min="1" max="50000" value="{{ request('min_price', 1) }}" id="slider-1" />
+                    <input type="range" min="1" max="50000" value="{{ request('max_price', 10000) }}" id="slider-2" />
+                </div>
+            </div>
+        </form>
+        <div class="sub-category-item">
+            <div class="card-list" id="view-list">
+                @if ($products && $products->isNotEmpty()) @foreach ($products as $product) @if ($product->status == 'approved')
+                <div class="item-card">
+                    <a href="{{ route('product.show', $product->id) }}" class="right">
+                        <img loading="lazy" src="{{ asset('assets/products/' . $product->product_image) }}" class="card-img-top" alt="{{ $product->name }}" />
+                    </a>
+                    <div class="left pt-3">
+                        {{--
+                        <p class="price">¥{{ number_format($product->product_price) }}</p>
+                        --}}
+                        <p class="price">
+                            @if ($product->discount > 0)
+                            <span class="format me-2">{{ number_format($product->product_price - $product->discount, 0) }}</span>
+                            <span class="original-price format">{{ number_format($product->product_price, 0) }}</span>
+                            @else
+                            <span class="format">{{ number_format($product->product_price, 0) }}</span>
+                            @endif
+                        </p>
+                        <div class="title-category mb-2">
+                            <a href="{{ route('sub-category.show', $product->subCategory->id) }}" class="menu-category">{{ $product->subCategory->name }}</a>
+                            <h3 class="title">{{ $product->name }}</h3>
+                        </div>
+                        <a href="{{ route('product.show', $product->id) }}" class="txt m-b-10 description">
+                            {{ $product->description }}
+                        </a>
+                        <div class="d-flex gap-2 card-btn m-t-10">
+                            <a href="javascript:void(0);" class="py-1 common-btn2 -solid cart-btn" data-id="{{ $product->id }}">
+                                <i class="fa-solid fa-cart-shopping"></i>
+                            </a>
+                            <a href="javascript:void(0);" class="py-1 common-btn2 white-list-btn @if ($product->inWhiteLists()) active @endif" data-id="{{ $product->id }}">
+                                <i class="fa-solid fa-bookmark"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                @endif @endforeach @else
+                <h3 class="title">{{ trans_lang('no_products') }}</h3>
+                @endif
+            </div>
+        </div>
+
+        <!-- Pagination -->
+        <div class="row mt-4">
+            @if ($products->hasPages())
+            <ul class="pagination">
+                <!-- Previous Page Link -->
+                @if ($products->onFirstPage())
+                <li class="disabled">&lt;</li>
+                @else
+                <li><a href="{{ $products->previousPageUrl() }}" class="">&lt;</a></li>
+                @endif
+
+                <!-- Page Numbers (Only Show 3 Pages at a Time) -->
+                @php $start = max(1, $products->currentPage() - 1); $end = min($start + 2, $products->lastPage()); @endphp @for ($i = $start; $i <= $end; $i++)
+                    <li class="{{ $products->currentPage() == $i ? 'active' : '' }}">
+                    <a href="{{ $products->url($i) }}" class="">{{ $i }}</a>
+                    </li>
+                    @endfor
+
+                    <!-- Next Page Link -->
+                    @if ($products->hasMorePages())
+                    <li><a href="{{ $products->nextPageUrl() }}" class="">&gt;</a></li>
+                    @else
+                    <li class="disabled">&gt;</li>
+                    @endif
+            </ul>
+            @endif
+        </div>
+        {{--
+        <div class="pagination mt-2">
+            {{ $products->links('vendor.pagination.bootstrap-4') }}
+    </div>
+    --}} {{-- {{ $products->links() }} --}}
+</div>
+</div>
+
+<script src="{{ asset('assets/js/slider.js') }}"></script>
+<script src="{{ asset('assets/js/price-range.js') }}"></script>
+<script src="{{ asset('assets/js/pagination.js') }}"></script>
+<script src="{{ asset('assets/js/notify.js') }}"></script>
+
+<!-- <script>
         const itemsPerPage = 24;
         const items = document.querySelectorAll(".item-card");
         const pagination = document.getElementById("pagination");
@@ -249,15 +213,13 @@
         showPage(1);
     </script> -->
 
-    <script src="{{ asset('assets/js/view-list.js') }}"></script>
+<script src="{{ asset('assets/js/view-list.js') }}"></script>
 
-
-@endsection
-@section('script')
-    <script>
-        $(document).ready(() => {
-            handleAddToCartBtn('cart-btn');
-            handleAddToWhiteListBtn('white-list-btn');
-        })
-    </script>
+@endsection @section('script')
+<script>
+    $(document).ready(() => {
+        handleAddToCartBtn("cart-btn");
+        handleAddToWhiteListBtn("white-list-btn");
+    });
+</script>
 @endsection
