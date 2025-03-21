7817f7c52ccb59f57fa41922152f21fe8f02c8c4
swethaharhtun <116005543+swethaharhtun@users.noreply.github.com>
2025-03-21 21:32:12 +0630

    Update order_completed_buyer.blade.php

--- resources/views/emails/order_completed_buyer.blade.php
+++ resources/views/emails/order_completed_buyer.blade.php
@@ -1,37 +1,39 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Order Completed</title>
 </head>
+
 <body>
-    <h1>{{ trans_lang('order_completed') }}</h1>
-    <p>{{ trans_lang('hello') }}, {{ $user->username }}!</p>
-    <p>{{ trans_lang('your_order_has_been_successfully_completed') }}.</p>
+    <h1>{{ trans_lang('購入が完了しました') }}</h1>
+    <p>{{ $user->username }}様</p>
+    <p>いつもお世話になっております。この度は商品のご購入を頂き有難うございます。</p>
+    <p>なお、購入内容は以下となります。以下指定口座に振込の程、宜しくお願い致します</p>
 
-    <h2>Buyer Information</h2>
-    <p><strong>Name:</strong> {{ $user->name }}</p>
-    <p><strong>Email:</strong> {{ $user->email }}</p>
+    <h2>購入詳細</h2>
+    <p style="color: red;">{{ trans_lang('product_name') }}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{ trans_lang('price') }}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{ trans_lang('小計') }}</p>
+    @foreach ($carts as $item)
 
-    <h2>Order Details</h2>
-    <table border="1" width="100%" style="border-collapse: collapse; text-align: left;">
-        <thead>
-            <tr>
-                <th scope="col">{{ trans_lang('product_name') }}</th>
-                <th scope="col">{{ trans_lang('price') }}</th>
-                <th scope="col">{{ trans_lang('小計') }}</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($carts as $item)
-            <tr>
-                <td>{{ $item->product->name }}</td>
-                <td>¥{{ number_format($item->product->getSellPrice(), 0) }}</td>
-                <td>¥{{ number_format(($item->product->getSellPrice() * $item->quantity), 0) }}</td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
+    <p>{{ $item->product->name }}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;¥{{ number_format($item->product->getSellPrice(), 0) }}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;¥{{ number_format(($item->product->getSellPrice() * $item->quantity), 0) }}</p>
 
-    <p>{{ trans_lang('thank_you_for_shopping_with_us') }}!</p>
+    @endforeach
+    @php
+        $total = $carts->sum(function ($cart) {
+            return $cart->product->getSellPrice() * $cart->quantity;
+        }) ?? 0;
+    @endphp
+    <p>{{ trans_lang('total') }} : &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;¥{{ number_format($total, 0) }}</p>
+
+    <p>銀行口座</p>
+    <p>◯◯銀行　普通　口座番号　◯◯◯◯◯◯</p>
+    <p>振込確認次第、発送日等のご連絡をさせて頂きます。
+    引き続き宜しくお願い致します。</p>
+    <p>＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝</p>
+    <p>株式会社Acompany</p>
+    <p>〒817-0702</p>
+    <p>長崎県対馬市上対馬町古里13-3</p>
+    <p>電話番号 : 0920-86-4516</p>
 </body>
+
 </html>
