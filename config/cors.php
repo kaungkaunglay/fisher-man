0660e8afb07a85df29bc7bde5e40937583001145
kaungkhantzin <you@example.com>
2025-04-23 14:10:04 +0630

    search error reform cross

--- config/cors.php
+++ config/cors.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-
     /*
     |--------------------------------------------------------------------------
     | Cross-Origin Resource Sharing (CORS) Configuration
@@ -15,20 +14,12 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
-    'allowed_methods' => ['*'],
-
-    'allowed_origins' => ['*'],
-
+    'paths' => ['fetch-data-search', 'api/*', 'sanctum/csrf-cookie'],
+    'allowed_methods' => ['GET', 'OPTIONS'],
+    'allowed_origins' => ['http://127.0.0.1:8000', 'http://localhost:8000'],
     'allowed_origins_patterns' => [],
-
-    'allowed_headers' => ['*'],
-
+    'allowed_headers' => ['Accept'],
     'exposed_headers' => [],
-
     'max_age' => 0,
-
     'supports_credentials' => false,
-
 ];
